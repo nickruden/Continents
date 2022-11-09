2d5920a7d3f5e1ed85a02a56e4a9faf7d547f0b1
nickruden <1nikitos.ru2018@gmail.com>
2022-11-09 18:28:22 +0300

    Созданы адреса страниц

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,34 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Евразия */
+router.get('/eurasia', function(req, res, next){
+  res.send("<h1>Континент Евразия</h1>")
+});
+
+/* Африка */
+router.get('/africa', function(req, res, next){
+  res.send("<h1>Континент Африка</h1>")
+});
+
+/* Северная Америка */
+router.get('/north_america', function(req, res, next){
+  res.send("<h1>Континент Северная Америка</h1>")
+});
+
+/* Южная Америка */
+router.get('/south_america', function(req, res, next){
+  res.send("<h1>Континент Южная Америка</h1>")
+});
+
+/* Австралия */
+router.get('/australia', function(req, res, next){
+  res.send("<h1>Континент Австралия</h1>")
+});
+
+/* Антарктида */
+router.get('/antarctica', function(req, res, next){
+  res.send("<h1>Континент Антарктида</h1>")
+});
+
 module.exports = router;
