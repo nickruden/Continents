7494701fbb56a7aac7a567edd9ff6500d13d9a97
nickruden <1nikitos.ru2018@gmail.com>
2022-12-01 20:45:59 +0300

    Заполнение меню из базы данных

--- routes/continents.js
+++ routes/continents.js
@@ -1,28 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var Continent = require("../models/continent").Continent;
+var async = require("async");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с continents');
+  res.send('Новый маршрутизатор, для маршрутов, начинающихся с continents')
 });
 
 /* Страница континентов */
 router.get('/:nick', function(req, res, next) {
-  console.log('в роутере')
-  console.log(req.params.nick)
-  Continent.findOne({ nick: req.params.nick}, function(err, Continent) {
+  async.parallel([
+    function(callback){
+      Continent.findOne({nick:req.params.nick}, callback)},
+    function(callback){
+      Continent.find({},{_id:0,title:1,nick:1}, callback)}
+],
+  function(err,result){
     if(err) return next(err)
+    var Continent = result[0]
+    var Continents = result[1] || []
     if(!Continent) return next(new Error("Нет такого континента на Земле!"))
-    res.render('continent', {
+    res.render('Continent', {
       title: Continent.title,
       picture: Continent.avatar,
-      desc: Continent.desc
-    })
+      desc: Continent.desc,
+      menu: Continents
+    });
   })
-}) 
-
-
+})
 
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Continent = require("../models/continent").Continent
 
 /* Главная */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
-    title: 'Express',
-    text: 'Проект "Континенты"'
+  Continent.find({}, {_id:0, title:1, nick:1}, function(err, menu){
+    res.render('index', { 
+      title: 'Express',
+      text: 'Проект "Континенты"',
+      menu: menu
+    })
   });
 });
 
