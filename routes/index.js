add4a23ad4f81e069dde9f5b2481fdcb83eacba4
nickruden <1nikitos.ru2018@gmail.com>
2022-12-04 12:25:24 +0300

    Команда записи в cookie

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var Continent = require("../models/continent").Continent
 /* Главная */
 router.get('/', function(req, res, next) {
   Continent.find({}, {_id:0, title:1, nick:1}, function(err, menu){
-    res.render('index', { 
+    res.cookie('gretting', 'Hi!!!').render('index', { 
       title: 'Express',
       text: 'Проект "Континенты"',
       menu: menu
