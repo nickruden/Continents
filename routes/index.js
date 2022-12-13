d5660482f612c7d22dd24d9d864e72638998dec1
nickruden <1nikitos.ru2018@gmail.com>
2022-12-13 16:17:43 +0300

    Логика пользователя

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Continent = require("../models/continent").Continent
+var User = require("./../models/user").User
 
 /* Главная */
 router.get('/', function(req, res, next) {
@@ -25,6 +26,25 @@ router.get('/logreg', function(req, res, next){
 router.post('/logreg', function(req, res, next){
   var username = req.body.username;
   var password = req.body.password;
-})
+
+  User.findOne({username:username},function(err,user){
+    if(err) return next(err)
+    if(user){
+	    if(user.checkPassword(password)){
+        req.session.user = user._id
+        res.redirect('/')
+      } 
+      else { res.render('logreg', {title: 'Вход'})
+      }
+    } else {
+	    var user = new User({username:username,password:password})
+      user.save(function(err,user){
+        if(err) return next(err)
+          req.session.user = user._id
+          res.redirect('/')
+      })
+    }
+  })
+});
 
 module.exports = router;
