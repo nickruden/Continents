3005b1a1e4cc28f8b87cecc8150951a692f3c380
nickruden <1nikitos.ru2018@gmail.com>
2023-01-07 18:00:26 +0300

    MySQL Заполнение меню из базы

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -1,11 +1,18 @@
-var Continent = require("./../models/continent").Continent
+// var Continent = require("./../models/continent").Continent
+var db = require('../mySQLConnect')
 
 module.exports = function(req, res, next){
     res.locals.nav = []
   
-    Continent.find(null,{_id:0, title:1, nick:1}, function(err, result){
+    db.query(`SELECT title, nick FROM continents`, (err, result) => {
       if(err)throw err
       res.locals.nav = result
       next()
     })
+
+    // Continent.find(null,{_id:0, title:1, nick:1}, function(err, result){
+    //   if(err)throw err
+    //   res.locals.nav = result
+    //   next()
+    // })
   }
