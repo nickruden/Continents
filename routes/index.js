98e1d94aba039dfeda1fd1feb08233894a92d633
nickruden <1nikitos.ru2018@gmail.com>
2023-01-14 14:51:18 +0300

    Глобальная переменная для навигации

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var Continent = require("../models/continent").Continent
 
 /* Главная */
 router.get('/', function(req, res, next) {
-  Continent.find({}, {_id:0, title:1, nick:1}, function(err, menu){
     req.session.greeting = "Hi!!!",
     res.cookie('gretting', 'Hi!!!').render('index', { 
       title: 'Express',
       text: 'Проект "Континенты"',
-      menu: menu,
       counter: req.session.counter
-    })
-  });
+  })
 });
 
 module.exports = router;
